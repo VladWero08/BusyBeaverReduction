90d23b6cded8343c6845519f36117e15b2db3bbd
VladWero08 <vlad_wero@yahoo.com>
2024-03-01 03:57:36 -0800

    added database container, which will be run with docker-compose

---
+++ database/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS `busy-beaver`;
+USE `busy-beaver`;
+
+CREATE TABLE IF NOT EXISTS `turing_machines` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `transition_function` text NOT NULL,
+    `number_of_states` tinyint NOT NULL,
+    `number_of_symbols` tinyint NOT NULL,
+    `halted` tinyint NOT NULL,
+    `steps` bigint NOT NULL,
+    `score` int NOT NULL,
+    `time_to_run` int NOT NULL,
+    
+    PRIMARY KEY (`id`)
+);
