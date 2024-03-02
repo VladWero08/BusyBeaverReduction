447918b04a48f14a2ae679edfca8104ce3a024a3
VladWero08 <vlad_wero@yahoo.com>
2024-03-02 12:02:59 -0800

    added select query for turing machines in database manager

--- busy_beaver_reduction/src/database/manager.rs
+++ busy_beaver_reduction/src/database/manager.rs
@@ -2,9 +2,10 @@ use dotenv::dotenv;
 use log::{error, info};
 use std::env;
 
-use sqlx::Pool;
-use sqlx::mysql::{MySql, MySqlPoolOptions, MySqlQueryResult};
+use sqlx::mysql::{MySql, MySqlPoolOptions, MySqlQueryResult, MySqlRow};
+use sqlx::{Pool, Row};
 
+use crate::delta::transition_function::TransitionFunction;
 use crate::turing_machine::turing_machine::TuringMachine;
 
 const MAX_POOL_CONNECTIONS: u32 = 8;
@@ -18,12 +19,12 @@ impl DatabaseManager {
     pub async fn new() -> Option<Self> {
         let connection_string = DatabaseManager::get_connection_string();
         let pool = DatabaseManager::get_pool(&connection_string).await;
-        
+
         match pool {
             Ok(pool) => {
                 return Some(DatabaseManager {
                     connection_string: connection_string,
-                    pool: pool
+                    pool: pool,
                 })
             }
             Err(error) => {
@@ -54,7 +55,7 @@ impl DatabaseManager {
     }
 
     /// Gets the `pool` of connections using the `connection_string`.
-    async fn get_pool(connection_string: &String) -> Result<Pool<MySql>, sqlx::Error>{
+    async fn get_pool(connection_string: &String) -> Result<Pool<MySql>, sqlx::Error> {
         let pool = MySqlPoolOptions::new()
             .max_connections(MAX_POOL_CONNECTIONS)
             .connect(&connection_string)
@@ -63,7 +64,64 @@ impl DatabaseManager {
         Ok(pool)
     }
 
-    /// Using the `pool` of connections, insert the given `TuringMachine` 
+    /// Given a number of states and a number of symbols,
+    /// selects all the turing machines with a transtion functions
+    /// that matches those numbers, and returns a `Vec<TuringMachines>`
+    /// with all of them.
+    pub async fn select_turing_machines(
+        &mut self,
+        number_of_states: u8,
+        number_of_symbols: u8,
+    ) -> Option<Vec<TuringMachine>> {
+        let result: Result<Vec<MySqlRow>, sqlx::Error> = sqlx::query(
+            "
+                SELECT * 
+                FROM turing_machines 
+                WHERE number_of_states = ? 
+                    AND number_of_symbols = ?",
+        )
+        .bind(number_of_states)
+        .bind(number_of_symbols)
+        .fetch_all(&self.pool)
+        .await;
+
+        match result {
+            Ok(rows) => {
+                let mut turing_machines = Vec::<TuringMachine>::new();
+
+                for row in rows {
+                    // reconstruct the transition function
+                    let transition_function_encoded = row.get(1);
+
+                    let mut transition_function =
+                        TransitionFunction::new(number_of_states, number_of_symbols);
+                    // decode the transition function
+                    transition_function.decode(transition_function_encoded);
+
+                    // reconstruct the turing machine
+                    let mut turing_machine = TuringMachine::new(transition_function);
+                    turing_machine.halted = row.get(4);
+                    turing_machine.steps = row.get(5);
+                    turing_machine.score = row.get(6);
+                    turing_machine.runtime = row.get(7);
+
+                    // increase the vector of turing machines from the database
+                    turing_machines.push(turing_machine);
+                }
+
+                return Some(turing_machines);
+            }
+            Err(error) => {
+                error!(
+                    "While selecting all turing machines from database: {}",
+                    error
+                );
+                return None;
+            }
+        }
+    }
+
+    /// Using the `pool` of connections, insert the given `TuringMachine`
     /// into the `turing_machines` table.
     pub async fn insert_turing_machine(&mut self, turing_machine: TuringMachine) {
         // get the encoding of the transition function, as a string,
